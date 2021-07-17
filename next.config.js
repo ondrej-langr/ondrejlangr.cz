1f0b40575c7d22b887b92e0f845f62efc3a1bd4e
Ondřej Langr <hi@ondrejlangr.cz>
2021-07-17 11:32:51 +0200

    feat: dropped pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const withPWA = require("next-pwa");
-
 module.exports = withPWA({
   webpack5: true,
   i18n: {
     locales: ["en-US", "cs-CZ"],
     defaultLocale: "en-US",
-  },
-  pwa: {
-    dest: "public",
-    disable: true || process.env.NODE_ENV === "development",
   },
 });
