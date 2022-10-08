4700cb724f640d10239904f0ae5cd88269d95be8
Ondřej Langr <hi@ondrejlangr.cz>
2022-10-08 21:10:25 +0200

    Move some assets under src folder and change import in constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,45 +5,45 @@ export const projectsList = [
     title: 'Larokinvest',
     url: 'https://larokinvest.cz/',
     tags: [Tags['custom-cms'], Tags.webpage],
-    imageUrl: require('@public-files/work/larok.png'),
+    imageUrl: require('@/assets/projects/work/larok.png'),
   },
   {
     title: 'Pohadkovyzamek',
     url: 'https://pohadkovy-zamek.cz/',
     tags: [Tags.eshop, Tags.webpage],
-    imageUrl: require('@public-files/work/zamek.png'),
+    imageUrl: require('@/assets/projects/work/zamek.png'),
   },
 
   {
     title: 'Plantae',
     url: 'https://plantae-lukavec.cz/obchod/',
     tags: [Tags.eshop, Tags.webpage],
-    imageUrl: require('@public-files/work/plantae.png'),
+    imageUrl: require('@/assets/projects/work/plantae.png'),
   },
   {
     title: 'Badadovolena',
     url: 'http://badadovolena.cz/hlavni/',
     tags: [Tags.webpage],
-    imageUrl: require('@public-files/work/badadovolena.png'),
+    imageUrl: require('@/assets/projects/work/badadovolena.png'),
   },
 
   {
     title: 'Decorasi',
     url: 'http://decorasi.cz/',
     tags: [Tags.webpage, Tags.eshop],
-    imageUrl: require('@public-files/work/decorasi.png'),
+    imageUrl: require('@/assets/projects/work/decorasi.png'),
   },
   {
     title: 'Svobodnilezci',
     url: 'https://www.svobodnilezci.com/',
     tags: [Tags.webpage, Tags.blog],
-    imageUrl: require('@public-files/work/svobodni.png'),
+    imageUrl: require('@/assets/projects/work/svobodni.png'),
   },
 
   {
     title: 'Pragaleto',
     url: 'https://pragaleto.cz/',
     tags: [Tags.webpage],
-    imageUrl: require('@public-files/work/pragaleto.png'),
+    imageUrl: require('@/assets/projects/work/pragaleto.png'),
   },
 ];
