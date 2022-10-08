41d80c0d4708aaab9e8bec23e3df08bddde586b9
Ondřej Langr <hi@ondrejlangr.cz>
2022-10-08 15:12:35 +0200

    Fix and rewrite some text parts

--- src/components/pages/main/sections/About.tsx
+++ src/components/pages/main/sections/About.tsx
@@ -5,20 +5,20 @@ export const AboutSection: FC = () => {
   return (
     <TitledSection title="About me" id="about-me">
       <p className="text-gray-500 text-opacity-80 text-2xl xsm:text-3xl xsm:leading-10 mt-10 !leading-relaxed">
-        Hey 👋 Im a full-stack developer who loves to do things right for more
+        Hey 👋 Im a full-stack developer who loves to do things right for over
         than {new Date().getFullYear() - 2016} years now. I always seek the
         environment where can I do so and try to surround myself with people who
-        thinks the same 🚀. <br />
+        thinks the same 🚀 <br />
         <br />
         When I don't work I focus on my hobbies and my family. My hobbies are
-        book reading, cooking, extreme sports, socializing and learning new
-        languages or broadening my mind 🌅.
+        reading, cooking, extreme sports, socializing and learning new
+        languages. AKA avid mind-broadening person 🌅
         <br />
         <br />
-        My favorite topics are car tuning, psychology, japanese culture and, of
-        course, programming and technology ⚙️. <br />
+        My favorite topics are, of course, latest technology and programming
+        trends, cars, psychology and japanese culture 🙇 <br />
         <a className="bold" href="#contact">
-          Seems interesting? Contact me then 😀
+          Want to talk it over coffee? Contact me then 😀
         </a>
       </p>
     </TitledSection>
--- src/components/pages/main/sections/WhatCanIOffer.tsx
+++ src/components/pages/main/sections/WhatCanIOffer.tsx
@@ -9,16 +9,15 @@ export const WhatCanIOfferSection: FC = (): ReactElement => {
     <TitledSection title={t('What can I do for you')} id="what-can-i-offer">
       <p className="text-gray-500 text-opacity-80 text-2xl xsm:text-3xl xsm:leading-10 mt-10 !leading-relaxed">
         Let's get into the real stuff. For starters I'm very open person who
-        loves to innovate and try new things - that means, when working with me,
-        you are going to things that are important. <b>Your idea.</b>
+        loves to innovate and try new things. What that really means, when
+        working with me, you are going to get done things that are important.
         <br />
         <br />
-        Every special idea needs special solution and for that we need special
-        tools 😇. <br /> I usually go with Javascript <b>(Typescript only)</b>
-        <br />
-        for entire codebase, because I find it the most <b>productive 💪</b>,
-        but I have a wide range of expertise on other technologies as well 😎.
-        In the end its not about the language since its just a tool 🤫. <br />
+        Every special project needs special solution and for that it needs
+        special tools ⚒️ <br /> My usual go-to for web apps is Javascript{' '}
+        <b>(Typescript only)</b>
+        <br />, because I find it the most <b>productive 💪</b>, but I have a
+        wide range of expertise on other technologies as well 😎 <br />
         <a className="bold" href="#contact">
           Thirsty for more? I gotchu - lets talk! 🔥
         </a>
