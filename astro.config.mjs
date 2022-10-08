1856af6a1478ecc281c816e2cf8e125d40cd9d40
Ondřej Langr <hi@ondrejlangr.cz>
2022-10-08 17:02:53 +0200

    Initiate and use astro instead of nextjs

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import image from '@astrojs/image';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    react(),
+    tailwind(),
+    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
+  ],
+});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
