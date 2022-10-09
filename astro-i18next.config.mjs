5718340f13039244da229df606ec28ac80574893
Ondřej Langr <hi@ondrejlangr.cz>
2022-10-09 11:33:45 +0200

    Use react i18next

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -3,7 +3,7 @@ export default {
   defaultLanguage: 'en',
   supportedLanguages: ['en', 'cs'],
   i18next: {
-    debug: true, // convenient during development to check for missing keys
+    debug: false, // convenient during development to check for missing keys
     resources: {
       en: {
         translation: {
@@ -16,5 +16,8 @@ export default {
         },
       },
     },
+  },
+  i18nextPlugins: {
+    '{initReactI18next}': 'react-i18next',
   },
 };
