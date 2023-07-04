6e4780fe2658fce26fcab3df35b900f54f733d43
Ondřej Langr <hi@ondrejlangr.cz>
2023-07-05 01:00:12 +0200

    Update next config by adding redirects

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
-const { i18n } = require('./next-i18next.config');
-
-module.exports = {
-  i18n,
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = {
+  redirects: async () => [
+    {
+      source: '/github',
+      destination: 'https://github.com/ondrej-langr',
+      permanent: true,
+    },
+    {
+      source: '/linkedin',
+      destination: 'https://www.linkedin.com/in/langr-ondrej/',
+      permanent: true,
+    },
+  ],
 };
+
+module.exports = config;
